80485862114153680c9abd044e26637719c5d5a5
Evan D.F <hinavecode.dev@gmail.com>
2026-01-29 23:49:27 +0700

    YEs

--- Pages/Page 8/no3.js
+++ Pages/Page 8/no3.js
@@ -1,29 +1,76 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('yes');
-  
-    button.style.left = "50%";
-    button.style.top = "60%";
-  
+//Changing gif while hovering on a button
+const img = document.getElementById('image');
+
+const images = {
+    depressed: '../Assets/attitude.gif',
+    ori: "../Assets/angry.gif"
+};
+
+function ChangeImage(giphy) {
+    img.src = images[giphy];
+}
+function ResetImage() {
+    img.src = images.ori;
+};
+
+//BG image change while hovering on a button
+var body = document.body;
+
+function bg2() {
+	body.className = 'noo';
+};
+
+function resetBG(){
+	body.className = '';
+};
+
+//Moving button
+const buttonHeight = 50;
+const buttonWidth = 150;
+
+//Making bg
+function lines(){
+    let sizeW = Math.random() * 12;
+    let duration = Math.random() * 3;
+    let e = document. createElement('div');
+    e.setAttribute('class', 'circle');
+    document.body.appendChild(e);
+    e.style.width = 2*sizeW + 'px';
+    e.style.left = Math.random() * + innerWidth + 'px';
+    e.style.animationDuration = 2 + duration + 's';
+    setTimeout(function(){
+        document.body.removeChild(e)
+    }, 4000);
+}
+
+setInterval(function(){
+    lines();
+}, 200);
+
+const maxWidth = 650;
+const maxHeight = 600;
+
+window.addEventListener('DOMContentLoaded', () =>{
+    const button = document.getElementById('yes')
+
     button.addEventListener('mouseover', () => {
-      const maxX = window.innerWidth - button.offsetWidth;
-      const maxY = window.innerHeight - button.offsetHeight;
-  
-      const x = Math.random() * maxX;
-      const y = Math.random() * maxY;
-  
-      button.style.left = x + "px";
-      button.style.top = y + "px";
+        button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
+        button.style.bottom = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
     });
-  });
-  
-  /* LIGHT EFFECT FOLLOW CURSOR */
-  document.addEventListener('mousemove', e => {
-    document.documentElement.style.setProperty('--x', e.clientX + 'px');
-    document.documentElement.style.setProperty('--y', e.clientY + 'px');
-  });
-  
-  /* NO BUTTON */
-  function noFunction() {
+});
+
+//BG image change while hovering on a button
+var body = document.body;
+
+function bg() {
+	body.className = 'noo';
+};
+
+function resetBG(){
+	body.className = '';
+};
+
+//Linking to no page
+function noFunction() {
     window.location.href = "../Page 5/yes.html";
-  }
-  
+}
